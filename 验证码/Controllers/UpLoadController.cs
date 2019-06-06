f95b45c0e5833ff07325b1760e0512afbb049cc7
祖军 蒙 <943885179@qq.com>
2019-06-06 10:48:56 +0800

    上传图片加文字水印

--- 验证码/Controllers/UpLoadController.cs
+++ 验证码/Controllers/UpLoadController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -149,7 +151,7 @@ namespace 验证码.Controllers
             return Ok(new { count = files.Count, size, urlList });
         }
         /// <summary>
-        /// 
+        /// 下载图片
         /// </summary>
         /// <param name="fileName">文件名称(加后缀)1.png</param>
         /// <param name="fileTypeRoot">图片所在路径</param>
@@ -169,6 +171,64 @@ namespace 验证码.Controllers
                 // return new NotFoundResult();
                  return new StatusCodeResult(404);
                // return new BadRequestObjectResult(ex.ToString());
+            }
+        }
+        /// <summary>
+        /// 添加文字水印
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="fileTypeRoot"></param>
+        /// <returns></returns>
+        [HttpPost("UploadAddText")]
+        public IActionResult UploadAddText(IFormFile file,string fileTypeRoot="upload")
+        {
+            try
+            {
+                if (null == file)
+                {
+                    return BadRequest();
+                }
+                if (file.Length > 0)
+                {
+                    var fileDir = fileRoot + fileTypeRoot + "/" + DateTime.Now.Year + "/" + DateTime.Now.Month + "/" + DateTime.Now.Day;
+                    if (!Directory.Exists(fileDir))
+                    {
+                        Directory.CreateDirectory(fileDir);
+                    }
+                    //string fileName = file.FileName;
+                    //string filePath= fileDir + $@"\{fileName}";
+                    var fileName = DateTime.Now.ToString("yyyyMMddHHmmss") +
+                                 Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(fileDir, fileName);
+                    using (var stream = new MemoryStream())
+                    {
+                        file.CopyTo(stream);
+                        // Add watermark
+                        var watermarkedStream = new MemoryStream();
+                        using (var img = Image.FromStream(stream))
+                        {
+                            using (var graphic = Graphics.FromImage(img))
+                            {
+                                var font = new Font(FontFamily.GenericSansSerif, 20, FontStyle.Bold, GraphicsUnit.Pixel);
+                                var color = Color.Red;//FromArgb(128, 255, 255, 255);
+                                var brush = new SolidBrush(color);
+                                var point = new Point(img.Width - 120, img.Height - 30);
+
+                                graphic.DrawString("weixiao", font, brush, point);
+                                img.Save(watermarkedStream, ImageFormat.Png);
+                            }
+                            img.Save(filePath);
+
+                        }
+                    }
+                    return Ok(new { code = 200, url = this.Request.Scheme + "://" + this.Request.Host + "/" + fileTypeRoot + "/" + DateTime.Now.Year + "/" + DateTime.Now.Month + "/" + DateTime.Now.Day + "/" + fileName });
+                    // return StatusCode(StatusCodes.Status200OK);
+                }
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
     }
